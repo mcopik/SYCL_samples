ba28f1441ecaa150f911b3eb0189736de6cf24ad
mcopik <mcopik@gmail.com>
2016-05-31 16:18:53 -0500

    better description

--- problems/device_list.cpp
+++ problems/device_list.cpp
@@ -4,7 +4,8 @@
 
 int main()
 {
-    std::vector<cl::sycl::device> devices = cl::sycl::device::get_devices( CL_DEVICE_TYPE_ALL /* sycl::info::device_type::all*/);
+    // Should be: sycl::info::device_type::all
+    std::vector<cl::sycl::device> devices = cl::sycl::device::get_devices( CL_DEVICE_TYPE_ALL );
     std::cout << devices.size() << " devices found by SYCL runtime" << std::endl;
     return 0;
 }
